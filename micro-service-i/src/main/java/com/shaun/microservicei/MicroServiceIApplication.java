dd77dc9d98ef34199a793d4724716fa5a3999022
shaun.zhou <shaun.zhou@aifmworld.com>
2020-03-31 10:39:49 +0800

    [Init]

---
+++ micro-service-i/src/main/java/com/shaun/microservicei/MicroServiceIApplication.java
@@ -0,0 +1,13 @@
+package com.shaun.microservicei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceIApplication.class, args);
+	}
+
+}
---
+++ micro-service-i/src/test/java/com/shaun/microservicei/MicroServiceIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.microservicei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9f1aaee1c59551875609b4d61094ed8fb9f9a34e
Shaun Chow <uranus-neptune@hotmail.com>
2020-04-27 16:50:47 +0800

    [Add] spring-security-authorization-server & spring-security-resource-server

---
+++ user-account-authentication/spring-security-authorization-server/src/test/java/com/shaun/useraccountauthentication/springsecurityauthorizationserver/SpringSecurityAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.useraccountauthentication.springsecurityauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

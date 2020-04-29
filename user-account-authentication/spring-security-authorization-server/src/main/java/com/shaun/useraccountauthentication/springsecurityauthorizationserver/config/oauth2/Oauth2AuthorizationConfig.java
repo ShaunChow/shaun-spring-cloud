34b4926c888746e467ffe651309b9c7e4f418292
Shaun Chow <uranus-neptune@hotmail.com>
2020-04-29 13:38:13 +0800

    [Fix] authorization server /oauth/check_token config

--- user-account-authentication/spring-security-authorization-server/src/main/java/com/shaun/useraccountauthentication/springsecurityauthorizationserver/config/oauth2/Oauth2AuthorizationConfig.java
+++ user-account-authentication/spring-security-authorization-server/src/main/java/com/shaun/useraccountauthentication/springsecurityauthorizationserver/config/oauth2/Oauth2AuthorizationConfig.java
@@ -11,6 +11,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
@@ -41,11 +45,24 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
         return new JdbcTokenStore(dataSource);
     }
 
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         super.configure(security);
         // This will enable /oauth/check_token access
-        security.checkTokenAccess("permitAll");
+        security.checkTokenAccess("isAuthenticated()");
+
+        // This will enable /oauth/token_key access
+        security.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')");
 
         // BCryptPasswordEncoder() is used for oauth_client_details.user_secret
         security.passwordEncoder(passwordEncoder);
@@ -58,7 +75,11 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(tokenStore());
+
+        endpoints.approvalStore(approvalStore())                        // oauth_approvals
+                .authorizationCodeServices(authorizationCodeServices()) // oauth_code
+                .tokenStore(tokenStore());                              // oauth_access_token & oauth_refresh_token
+
         endpoints.authenticationManager(authenticationManager);
         endpoints.userDetailsService(userDetailsService);
     }

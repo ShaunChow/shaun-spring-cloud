38dfdcab1e7765ca8f4e825cfeeb6784f39624ca
Shaun Chow <uranus-neptune@hotmail.com>
2020-05-06 18:32:56 +0800

    [Add] UAA - spring security oauth2 login server

---
+++ user-account-authentication/spring-security-login-server/src/main/java/com/shaun/useraccountauthentication/springsecurityloginserver/SpringSecurityLoginServerApplication.java
@@ -0,0 +1,13 @@
+package com.shaun.useraccountauthentication.springsecurityloginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginServerApplication.class, args);
+	}
+
+}
---
+++ user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/SpringSecurityLoginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.useraccountauthentication.springsecurityloginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8b9032c2781671fc85425cafe0de2f864d11d3b5
Shaun Chow <uranus-neptune@hotmail.com>
2020-05-08 01:38:29 +0800

    [Update] github oidc referene from https://spring.io/guides/tutorials/spring-boot-oauth2/

--- user-account-authentication/spring-security-login-server/src/main/java/com/shaun/useraccountauthentication/springsecurityloginserver/config/WebSecurityConfig.java
+++ user-account-authentication/spring-security-login-server/src/main/java/com/shaun/useraccountauthentication/springsecurityloginserver/config/WebSecurityConfig.java
@@ -1,18 +1,31 @@
 package com.shaun.useraccountauthentication.springsecurityloginserver.config;
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-@EnableWebSecurity(debug=true)
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
                 .oauth2Login();
     }
 

5275568649aa5fe9f0a60b018dd69c89be0b8623
Shaun Chow <uranus-neptune@hotmail.com>
2020-05-12 19:10:23 +0800

    [Update] h2

--- user-account-authentication/spring-security-resource-server/src/main/java/com/shaun/useraccountauthentication/springsecurityresourceserver/config/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.shaun.useraccountauthentication.springsecurityresourceserver.config;
-//
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity(debug=true)
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//}

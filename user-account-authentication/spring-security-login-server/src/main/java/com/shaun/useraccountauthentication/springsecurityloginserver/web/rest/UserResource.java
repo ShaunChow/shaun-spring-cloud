0058b9dc1f805cfe8c3f4b647ccf81b9e737b5b3
Shaun Chow <uranus-neptune@hotmail.com>
2020-05-15 15:02:21 +0800

    [Update] login server

--- user-account-authentication/spring-security-login-server/src/main/java/com/shaun/useraccountauthentication/springsecurityloginserver/web/rest/UserResource.java
+++ user-account-authentication/spring-security-login-server/src/main/java/com/shaun/useraccountauthentication/springsecurityloginserver/web/rest/UserResource.java
@@ -1,11 +1,13 @@
 package com.shaun.useraccountauthentication.springsecurityloginserver.web.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.Map;
@@ -13,10 +15,25 @@ import java.util.Map;
 @RestController
 public class UserResource {
 
+    @Value("${spring.security.oauth2.authorization.token-url}")
+    private String oauth2TokenUrl;
+
+    @Value("${spring.security.oauth2.authorization.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.authorization.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.authorization.grant-type}")
+    private String grantType;
+
     private final OAuth2AuthorizedClientService authorizedClientService;
 
-    public UserResource(OAuth2AuthorizedClientService authorizedClientService) {
+    private final RestTemplate restTemplate;
+
+    public UserResource(OAuth2AuthorizedClientService authorizedClientService, RestTemplate restTemplate) {
         this.authorizedClientService = authorizedClientService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/user")
@@ -44,5 +61,4 @@ public class UserResource {
                         + "}"
         );
     }
-
 }
--- user-account-authentication/spring-security-resource-server/src/main/resources/database/schema-h2.sql
+++ user-account-authentication/spring-security-resource-server/src/main/resources/database/schema-h2.sql
@@ -1,102 +1,13 @@
-
----- Spring Security ----
-
-/* 1. Default */
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `enabled` tinyint(4) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`username`)
-);
-
-DROP TABLE IF EXISTS `authorities`;
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL
-);
-CREATE UNIQUE INDEX ix_auth_username
-  on authorities (username,authority);
-
-/* 2. Groups */
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE `groups` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `group_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-DROP TABLE IF EXISTS `group_authorities`;
-CREATE TABLE `group_authorities` (
-  `group_id` bigint NOT NULL ,
-  `authority` varchar(50) NOT NULL
-);
-
-DROP TABLE IF EXISTS `group_members`;
-CREATE TABLE `group_members` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `group_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
----- Spring Oauth2 ----
-
-DROP TABLE IF EXISTS `oauth_client_token`;
-CREATE TABLE `oauth_client_token` (
-  `token_id` varchar(255),
-  `token` longblob,
-  `authentication_id` varchar(255),
-  `user_name` varchar(255),
-  `client_id` varchar(255)
-);
-
-DROP TABLE IF EXISTS `oauth_client_details`;
-CREATE TABLE `oauth_client_details` (
-  `client_id` varchar(255) NOT NULL,
-  `resource_ids` varchar(255) DEFAULT NULL,
-  `client_secret` varchar(255) DEFAULT NULL,
-  `scope` varchar(255) DEFAULT NULL,
-  `authorized_grant_types` varchar(255) DEFAULT NULL,
-  `web_server_redirect_uri` varchar(255) DEFAULT NULL,
-  `authorities` varchar(255) DEFAULT NULL,
-  `access_token_validity` integer(11) DEFAULT NULL,
-  `refresh_token_validity` integer(11) DEFAULT NULL,
-  `additional_information` varchar(255) DEFAULT NULL,
-  `autoapprove` varchar(255) DEFAULT NULL
-);
-
-DROP TABLE IF EXISTS `oauth_access_token`;
-CREATE TABLE `oauth_access_token` (
-  `token_id` varchar(255),
-  `token` longblob,
-  `authentication_id` varchar(255),
-  `user_name` varchar(255),
-  `client_id` varchar(255),
-  `authentication` longblob,
-  `refresh_token` varchar(255)
-);
-
-DROP TABLE IF EXISTS `oauth_refresh_token`;
-CREATE TABLE `oauth_refresh_token`(
-  `token_id` varchar(255),
-  `token` longblob,
-  `authentication` longblob
-);
-
-DROP TABLE IF EXISTS `oauth_code`;
-CREATE TABLE `oauth_code` (
-  `code` varchar(255),
-  `authentication` blob
-);
-
-DROP TABLE IF EXISTS `oauth_approvals`;
-CREATE TABLE `oauth_approvals` (
-    `userId` varchar(255),
-    `clientId` varchar(255),
-    `scope` varchar(255),
-    `status` varchar(10),
-    `expiresAt` datetime,
-    `lastModifiedAt` datetime
-);
-
+create table UserConnection (userId varchar(255) not null,
+	providerId varchar(255) not null,
+	providerUserId varchar(255),
+	rank int not null,
+	displayName varchar(255),
+	profileUrl varchar(512),
+	imageUrl varchar(512),
+	accessToken varchar(512) not null,
+	secret varchar(512),
+	refreshToken varchar(512),
+	expireTime bigint,
+	primary key (userId, providerId, providerUserId));
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);

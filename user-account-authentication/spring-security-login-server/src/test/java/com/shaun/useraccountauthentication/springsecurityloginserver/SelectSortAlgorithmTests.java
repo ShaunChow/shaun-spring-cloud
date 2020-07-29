fb9f6d32b61e452357322b59f619b50834281c17
Shaun Chow <uranus-neptune@hotmail.com>
2020-07-29 18:32:09 +0800

    [Update] Heapsort & Selection-Sort

--- user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/SelectSortAlgorithmTests.java
+++ user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/SelectSortAlgorithmTests.java
@@ -10,12 +10,42 @@ import java.util.function.BiFunction;
 @ActiveProfiles("test")
 @SpringBootTest
 public class SelectSortAlgorithmTests {
+
     int[] sourceArray = {2, 9, 4, 2, 2, 5, 1, 1, 2, 1, 9, 8, 4, 4, 9};
     int[] resultArray = {1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 5, 8, 9, 9, 9};
 
-    private void buildMaxHeap(int[] arr, int len) {
-        for (int i = (int) Math.floor(len / 2); i >= 0; i--) {
-            maxHeapify(arr, i, len);
+    BiFunction<int[], BiFunction<int[], Integer, Integer>, int[]> selectionSortMax = (arr, extractFunction) -> {
+        int len = arr.length;
+
+        for (int i = len - 1; i > 0; i--) {
+            extractFunction.apply(arr, i);
+        }
+        return arr;
+    };
+    BiFunction<int[], BiFunction<int[], Integer, Integer>, int[]> selectionSortMin = (arr, extractFunction) -> {
+        int len = arr.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            extractFunction.apply(arr, i);
+        }
+        return arr;
+    };
+
+    private void swap(int[] arr, int i, int j) {
+        if (i == j)
+            return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /*
+     * Heapsort
+     */
+
+    private void buildMaxHeap(int[] arr) {
+        for (int i = (int) Math.floor(arr.length / 2); i >= 0; i--) {
+            maxHeapify(arr, i, arr.length);
         }
     }
 
@@ -38,22 +68,14 @@ public class SelectSortAlgorithmTests {
         }
     }
 
-    private void swap(int[] arr, int i, int j) {
-        if (i == j)
-            return;
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
     @Test
-    void Heapsort() {
+    void HeapsortMax() {
 
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
         int len = arr.length;
 
-        buildMaxHeap(arr, len);
+        buildMaxHeap(arr);
 
         for (int i = len - 1; i > 0; i--) {
             swap(arr, 0, i);
@@ -64,39 +86,52 @@ public class SelectSortAlgorithmTests {
     }
 
     @Test
-    void SelectionMinSort() {
+    void HeapsortMaxII() {
 
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
         int len = arr.length;
 
-        for (int i = 0; i < len - 1; i++) {
-            int min = i;
+        buildMaxHeap(arr);
 
-            for (int j = i + 1; j < len; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
-            }
-
-            swap(arr, i, min);
+        for (int i = len - 1; i > 0; i--) {
+            heapsortExtractMax.apply(arr, i);
         }
         assert (Arrays.equals(arr, resultArray));
     }
 
     @Test
-    void SelectionMaxSort() {
-
+    void HeapsortMaxIII() {
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
-
-        int len = arr.length;
-
-        for (int i = len - 1; i > 0; i--) {
-            selectionSortExtractMax.apply(arr, i);
-        }
+        buildMaxHeap(arr);
+        arr = selectionSortMax.apply(arr, heapsortExtractMax);
         assert (Arrays.equals(arr, resultArray));
     }
 
+    BiFunction<int[], Integer, Integer> heapsortExtractMax = (arr, maxPosition) -> {
+        maxHeapify(arr, 0, maxPosition);
+        swap(arr, maxPosition, 0);
+        return arr[maxPosition];
+    };
+
+
+    /*
+     * Selection-Sort
+     */
+
+    @Test
+    void SelectionMinSort() {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        arr = selectionSortMin.apply(arr, selectionSortExtractMin);
+        assert (Arrays.equals(arr, resultArray));
+    }
+
+    @Test
+    void SelectionMaxSort() {
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        arr = selectionSortMax.apply(arr, selectionSortExtractMax);
+        assert (Arrays.equals(arr, resultArray));
+    }
 
     BiFunction<int[], Integer, Integer> selectionSortExtractMax = (arr, maxPosition) -> {
         int max = maxPosition;
@@ -109,5 +144,18 @@ public class SelectSortAlgorithmTests {
 
         swap(arr, maxPosition, max);
         return arr[maxPosition];
+    };
+
+    BiFunction<int[], Integer, Integer> selectionSortExtractMin = (arr, minPosition) -> {
+        int min = minPosition;
+
+        for (int j = minPosition + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        swap(arr, minPosition, min);
+        return arr[minPosition];
     };
 }

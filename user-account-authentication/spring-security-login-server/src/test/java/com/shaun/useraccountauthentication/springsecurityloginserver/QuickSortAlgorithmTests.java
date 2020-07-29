477f5e5be16429fe8281196173f318272c03d0a3
Shaun Chow <uranus-neptune@hotmail.com>
2020-07-29 19:21:42 +0800

    [Update] QuickSortAlgorithmTests

--- user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/QuickSortAlgorithmTests.java
+++ user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/QuickSortAlgorithmTests.java
@@ -9,6 +9,32 @@ public class QuickSortAlgorithmTests {
     int[] sourceArray = {2, 9, 4, 2, 2, 5, 1, 1, 2, 1, 9, 8, 4, 4, 9};
     int[] resultArray = {1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 5, 8, 9, 9, 9};
 
+    private void swap(int[] arr, int i, int j) {
+        if (i == j)
+            return;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    /*
+     * Bubble-Sort
+     */
+
     @Test
     void BubbleSort() {
 
@@ -29,6 +55,23 @@ public class QuickSortAlgorithmTests {
         assert (Arrays.equals(arr, resultArray));
     }
 
+    @Test
+    void BubbleSortII() {
+
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                int partitionIndex = partition(arr, j, arr.length - 1);
+            }
+        }
+        assert (Arrays.equals(arr, resultArray));
+    }
+
+    /*
+     * Quick-Sort
+     */
+
     private int[] quickSort(int[] arr, int left, int right) {
         if (left < right) {
             int partitionIndex = partition(arr, left, right);
@@ -38,21 +81,6 @@ public class QuickSortAlgorithmTests {
         return arr;
     }
 
-    private int partition(int[] arr, int left, int right) {
-        // 设定基准值（pivot）
-        int pivot = left;
-        int index = pivot + 1;
-        for (int i = index; i <= right; i++) {
-            if (arr[i] < arr[pivot]) {
-                swap(arr, i, index);
-                index++;
-            }
-        }
-        swap(arr, pivot, index - 1);
-        return index - 1;
-    }
-
-
     @Test
     void QuickSort() {
 
@@ -61,14 +89,5 @@ public class QuickSortAlgorithmTests {
         arr = quickSort(arr, 0, arr.length - 1);
 
         assert (Arrays.equals(arr, resultArray));
-    }
-
-
-    private void swap(int[] arr, int i, int j) {
-        if (i == j)
-            return;
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }

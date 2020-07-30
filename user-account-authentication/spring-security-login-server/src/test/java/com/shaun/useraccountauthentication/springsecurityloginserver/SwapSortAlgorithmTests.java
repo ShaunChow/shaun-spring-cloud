2715f8123fbade10db9cc8a4e348beb86fbdc3a9
Shaun Chow <uranus-neptune@hotmail.com>
2020-07-30 09:41:10 +0800

    [Name]

--- user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/SwapSortAlgorithmTests.java
+++ user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/SwapSortAlgorithmTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-public class QuickSortAlgorithmTests {
+public class SwapSortAlgorithmTests {
 
     int[] sourceArray = {2, 9, 4, 2, 2, 5, 1, 1, 2, 1, 9, 8, 4, 4, 9};
     int[] resultArray = {1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 5, 8, 9, 9, 9};

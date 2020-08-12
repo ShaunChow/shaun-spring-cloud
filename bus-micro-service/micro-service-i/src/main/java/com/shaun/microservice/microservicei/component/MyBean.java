d67996b121afa9f0250e9b967fdb1cc04a148ae3
Shaun Chow <uranus-neptune@hotmail.com>
2020-08-12 19:32:16 +0800

    [SetUp] springboot + java + kotlin (mix mode development)

--- bus-micro-service/micro-service-i/src/main/java/com/shaun/microservice/microservicei/component/MyBean.java
+++ bus-micro-service/micro-service-i/src/main/java/com/shaun/microservice/microservicei/component/MyBean.java
@@ -4,6 +4,8 @@ package com.shaun.microservice.microservicei.component;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component("myBean")
 public class MyBean {
 
@@ -11,7 +13,7 @@ public class MyBean {
     private String say;
 
     public String saySomething() {
-        return say;
+        return say + "-" + LocalDateTime.now().toString();
     }
 
 }

5bb20684ebc082beda0003efb27fe0206924c99e
Shaun Chow <uranus-neptune@hotmail.com>
2020-08-14 15:12:04 +0800

    [Add] Shaun's common lib

---
+++ common/src/test/java/com/shaun/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

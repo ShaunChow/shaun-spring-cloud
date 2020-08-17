dddd3e9a667e33a7018b48b01424bf1115442e66
Shaun Chow <uranus-neptune@hotmail.com>
2020-08-17 11:26:12 +0800

    [Add] java & kotlin mix unit tests

--- bus-micro-service/micro-service-i/src/test/java/com/shaun/microservice/microservicei/MicroServiceIApplicationTests.java
+++ bus-micro-service/micro-service-i/src/test/java/com/shaun/microservice/microservicei/MicroServiceIApplicationTests.java
@@ -1,13 +1,49 @@
 package com.shaun.microservice.microservicei;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
-class MicroServiceIApplicationTests {
+@RunWith(SpringRunner.class)
+public class MicroServiceIApplicationTests {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
-    void contextLoads() {
+    public void A_RESTFUL() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/")
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3))
+                .andDo(MockMvcResultHandlers.print());
     }
 
 }

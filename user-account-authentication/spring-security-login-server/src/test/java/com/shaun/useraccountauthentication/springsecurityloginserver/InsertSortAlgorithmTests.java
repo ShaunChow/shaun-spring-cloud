c7607cdb493bee309d79c41658212025ee00a974
Shaun Chow <uranus-neptune@hotmail.com>
2020-08-18 15:21:25 +0800

    [Update] InsertSort

--- user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/InsertSortAlgorithmTests.java
+++ user-account-authentication/spring-security-login-server/src/test/java/com/shaun/useraccountauthentication/springsecurityloginserver/InsertSortAlgorithmTests.java
@@ -13,27 +13,12 @@ class InsertSortAlgorithmTests {
     int[] sourceArray = {2, 9, 4, 2, 2, 5, 1, 1, 2, 1, 9, 8, 4, 4, 9};
     int[] resultArray = {1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 5, 8, 9, 9, 9};
 
-    int[] generalInsertSort(int[] arr, int gap) {
-        for (int i = gap; i < arr.length; i++) {
-            int tmp = arr[i];
-            int j = i - gap;
-            while (j >= 0 && tmp < arr[j]) {
-                arr[j + gap] = arr[j];
-                j -= gap;
-            }
-            if ((j + gap) != i) {
-                arr[j + gap] = tmp;
-            }
-        }
-        return arr;
-    }
-
     @Test
     void InsertSort() {
 
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
-        arr = generalInsertSort(arr, 1);
+        arr = GeneralSortAlgorithmUtil.InsertSort.apply(arr, 1);
 
         assert (Arrays.equals(arr, resultArray));
     }
@@ -49,7 +34,7 @@ class InsertSortAlgorithmTests {
         }
 
         while (gap > 0) {
-            arr = generalInsertSort(arr, gap);
+            arr = GeneralSortAlgorithmUtil.InsertSort.apply(arr, gap);
             gap = (int) Math.floor(gap / 3);
         }
 
@@ -65,7 +50,7 @@ class InsertSortAlgorithmTests {
 
         for (int i = 0; i < incrementSequence.length; i++) {
             int gap = incrementSequence[i];
-            arr = generalInsertSort(arr, gap);
+            arr = GeneralSortAlgorithmUtil.InsertSort.apply(arr, gap);
         }
 
         assert (Arrays.equals(arr, resultArray));

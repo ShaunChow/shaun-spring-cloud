f4b5e7eba056bc4cc6033615f3915ee0e76ae234
shaun_zhou <uranus-neptune@hotmail.com>
2020-09-14 09:44:28 +0800

    [Fix] properties with suffix of enable must been boolean

--- common/src/main/java/com/shaun/common/config/cors/CorsConfig.java
+++ common/src/main/java/com/shaun/common/config/cors/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@ConditionalOnExpression("'${setting.cors.enable}'.equals('on')")
+@ConditionalOnExpression("'${setting.cors.enable}'.equals('true')")
 @Configuration
 public class CorsConfig {
 

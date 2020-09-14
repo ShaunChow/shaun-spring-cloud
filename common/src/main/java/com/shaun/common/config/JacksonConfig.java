957d4f9a8fdcd05605ad25f8b75ac9108a67ed2c
shaun_zhou <uranus-neptune@hotmail.com>
2020-09-14 11:18:08 +0800

    [Update] add JacksonConfig 2 commmon

--- common/src/main/java/com/shaun/common/config/JacksonConfig.java
+++ common/src/main/java/com/shaun/common/config/JacksonConfig.java
@@ -1,21 +1,23 @@
-package com.shaun.microservice.microserviceii.config;
+package com.shaun.common.config;
 
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
 
+@ConditionalOnExpression("'${setting.jackson.enable}'.equals('true')")
 @Configuration
 public class JacksonConfig {
 
     @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
     private String localDateTimeFormat;
 
-    @Value("${spring.jackson.local-date-format:yyyy-MM-dd}")
+    @Value("${setting.jackson.local-date-format:yyyy-MM-dd}")
     private String localDateFormat;
 
     @Bean
@@ -27,3 +29,4 @@ public class JacksonConfig {
         };
     }
 }
+

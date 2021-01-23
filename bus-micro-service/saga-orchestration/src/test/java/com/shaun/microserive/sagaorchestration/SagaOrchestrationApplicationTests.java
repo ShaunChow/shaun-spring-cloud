0ebfb237d636a912cda0b393d17a7df1e57163ba
shaun_zhou <uranus-neptune@hotmail.com>
2021-01-24 01:11:05 +0800

    [Add] saga orchestration project

---
+++ bus-micro-service/saga-orchestration/src/test/java/com/shaun/microserive/sagaorchestration/SagaOrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaun.microserive.sagaorchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrchestrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
